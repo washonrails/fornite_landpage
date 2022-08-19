ecade74556d1bc7f8f5740a94c43a401420a48ac
Wallace Henrique <98850074+wAAAASH@users.noreply.github.com>
2022-08-18 21:20:15 -0300

    Add js

---
+++ js/main.js
@@ -0,0 +1,36 @@
+window.document.addEventListener('DOMContentLoaded', () => {
+
+    const bus = document.querySelector('.bus')
+    const spider = document.querySelector('.spider')
+    const container = document.querySelector('.container')
+    const containerSecond = document.querySelector('.container-secondary')
+    const containerThird = document.querySelector('.third-container')
+    const loader = document.querySelector('.rainbow-loader')
+
+    spider.style.display = 'none'
+    container.style.display = 'none'
+    containerSecond.style.display = 'none'
+    containerThird.style.display = 'none'
+
+    document.body.style.cssText =
+    'background: url("../assets/img/Main.jpg") center; background-size:cover;'
+
+
+    setTimeout(() => {
+        spider.style.display = 'none'
+        container.style.display = 'none'
+        containerSecond.style.display = 'flex'
+        containerThird.style.display = 'none'
+        loader.style.display = 'none'
+        document.body.style.cssText =
+         'background: url("../assets/img/DBZBackground.jpg") center; background-size: cover;'
+    }, 7500);
+})
+
+const soundParty = document.getElementById('party')
+soundParty.volume = 0.4
+
+
+setInterval(() => {
+    soundParty.volume -= 0.1;
+}, 1400)
